4aa55f0f5b8ca900e153f25ad859177edf6a27cb
Yoav Vainrich <yoavain@gmail.com>
2024-09-27 15:01:16 +0300

    Initial commit

--- src/store.ts
+++ src/store.ts
@@ -2,14 +2,18 @@ import { MilvusVectorStore, storageContextFromDefaults } from "llamaindex";
 import type { StorageContext } from "llamaindex/storage/StorageContext";
 import { getMilvusClient, getMilvusCollection } from "./vectorDb/milvus";
 
+const getVectorStore = (): MilvusVectorStore => {
+    return new MilvusVectorStore({
+        milvusClient: getMilvusClient(),
+        collection: getMilvusCollection()
+    });
+};
+
 let storageContext: StorageContext;
 export const getStorageContext = async (): Promise<StorageContext> => {
     if (!storageContext) {
         storageContext = await storageContextFromDefaults({
-            vectorStore: new MilvusVectorStore({
-                milvusClient: getMilvusClient(),
-                collection: getMilvusCollection()
-            })
+            vectorStore: getVectorStore()
         });
     }
     return storageContext;
