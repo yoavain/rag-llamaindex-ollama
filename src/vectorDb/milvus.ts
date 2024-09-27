c865b21070efcf4cad1060a46642fe95602062bd
Yoav Vainrich <yoavain@gmail.com>
2024-09-27 15:10:39 +0300

    Initial commit

--- src/vectorDb/milvus.ts
+++ src/vectorDb/milvus.ts
@@ -1,6 +1,10 @@
 import { MilvusClient } from "@zilliz/milvus2-sdk-node";
 import { milvus_address, milvus_collection_name } from "./consts";
 
+/*
+https://docs.llamaindex.ai/en/stable/examples/vector_stores/MilvusIndexDemo/
+ */
+
 // 1. Set up a Milvus Client
 const milvusClient: MilvusClient = new MilvusClient({ address: milvus_address });
 
