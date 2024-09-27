b1fb0beef3d131cbd4ccdd9d246177e8ee0105b5
Yoav Vainrich <yoavain@gmail.com>
2024-09-28 00:15:09 +0300

    trying Hebrew documents

--- src/ollamaGlobalSettings.ts
+++ src/ollamaGlobalSettings.ts
@@ -3,7 +3,8 @@ import { HuggingFaceEmbedding, Ollama, Settings } from "llamaindex";
 
 export const applyOllamaGlobals = () => {
     Settings.llm = new Ollama({
-        model: "llama3.1:8b"
+        // model: "llama3.1:8b"
+        model: "llama3.2:3b"
     });
 
     Settings.embedModel = new HuggingFaceEmbedding({
