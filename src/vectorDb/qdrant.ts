774d1c2d7d80165e39e9ec4c6e7a6237b729dcaa
Yoav Vainrich <yoavain@gmail.com>
2024-10-01 08:42:16 +0300

    Qdrant instead of Milvus

--- src/vectorDb/qdrant.ts
+++ src/vectorDb/qdrant.ts
@@ -1,10 +1,12 @@
 import { QdrantVectorStore } from "llamaindex";
 
 const QDRANT_ADDRESS = "http://localhost:6333";
+const QDRANT_COLLECTION_NAME = "example_collection";
 
 
 export const getQdrantVectorStore = (): QdrantVectorStore => {
     return new QdrantVectorStore({
-        url: QDRANT_ADDRESS
+        url: QDRANT_ADDRESS,
+        collectionName: QDRANT_COLLECTION_NAME
     });
 };
