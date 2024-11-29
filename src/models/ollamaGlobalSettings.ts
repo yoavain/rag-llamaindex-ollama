34a2cbe7363415449cfb24d9d0b16efcbbf4d189
Yoav Vainrich <yoavain@gmail.com>
2024-11-29 21:55:49 +0200

    Rename project

--- src/models/ollamaGlobalSettings.ts
+++ src/models/ollamaGlobalSettings.ts
@@ -7,7 +7,6 @@ export const applyOllamaGlobals = () => {
     });
 
     Settings.embedModel = new HuggingFaceEmbedding({
-        modelType: "BAAI/bge-small-en-v1.5",
-        quantized: false
+        modelType: "BAAI/bge-small-en-v1.5"
     });
 };
